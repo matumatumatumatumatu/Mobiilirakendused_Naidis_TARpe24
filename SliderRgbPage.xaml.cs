fa3a2ee3b4516e613abe8af9e3a4707f283a418a
opilane <opilane@HP-CZC2349HT2>
2026-02-19 09:37:18 +0200

    datetimepage, steppersliderpage tehtud, sliderrgbpage loodud

---
+++ SliderRgbPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Naidis_TARpe24;
+
+public partial class SliderRgbPage : ContentPage
+{
+	public SliderRgbPage()
+	{
+		
+	}
+}
--- StartPage.xaml.cs
+++ StartPage.xaml.cs
@@ -2,8 +2,8 @@ namespace Naidis_TARpe24;
 
 public partial class StartPage : ContentPage
 {
-	public List<ContentPage> lehed = new List<ContentPage>() { new TextPage(), new FigurePage(), new Timer_Page(), new ValgusfoorPage() };
-	public List<string> tekstid = new List<string> { "Tee lahti TextPage", "Tee lahti FigurePage", "Tee lahti TimerPage","Tee lahti ValgusfoorPage" };
+	public List<ContentPage> lehed = new List<ContentPage>() { new TextPage(), new FigurePage(), new Timer_Page(), new ValgusfoorPage(), new DateTimePage(), new StepperSliderPage(), new SliderRgbPage() };
+	public List<string> tekstid = new List<string> { "Tee lahti TextPage", "Tee lahti FigurePage", "Tee lahti TimerPage","Tee lahti ValgusfoorPage","Tee lahti DateTimePage","Tee lahti StepperSliderPage","Tee lahti SliderRgbPage" };
 	ScrollView sv;
 	VerticalStackLayout vst;
 	public StartPage()
