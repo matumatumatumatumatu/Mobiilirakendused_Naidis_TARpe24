47e02b9310c4de4c8030d2786336925b5c6eebfe
opilane <opilane@HP-CZC2349HT2>
2026-02-25 10:54:09 +0200

    täätab

--- SliderRgbPage.xaml.cs
+++ SliderRgbPage.xaml.cs
@@ -10,13 +10,34 @@ public partial class SliderRgbPage : ContentPage
     Slider slider1;
     Slider slider2;
     Slider slider3;
+    Button juhuslik;
+    Random rnd = new Random();
     AbsoluteLayout al;
     public SliderRgbPage()
 	{
         label = new Label
         {
-            Text = "...",
+            Text = "",
             BackgroundColor = Colors.LightGray,
+            WidthRequest = 100
+        };
+        labelR = new Label
+        {
+            Text = "",
+            BackgroundColor = Colors.Transparent,
+            WidthRequest = 50
+        };
+        labelG = new Label
+        {
+            Text = "",
+            BackgroundColor = Colors.Transparent,
+            WidthRequest = 50
+        };
+        labelB = new Label
+        {
+            Text = "",
+            BackgroundColor = Colors.Transparent,
+            WidthRequest = 50
         };
         slider1 = new Slider
         {
@@ -54,24 +75,60 @@ public partial class SliderRgbPage : ContentPage
         slider1.ValueChanged += Slider_Color;
         slider2.ValueChanged += Slider_Color;
         slider3.ValueChanged += Slider_Color;
-        al = new AbsoluteLayout { Children = { label,labelR, slider1,labelG,slider2,labelB,slider3 } };
-        List<View> controls = new List<View> { label,labelR, slider1,labelG,slider2,labelB,slider3 };
+        stepper = new Stepper
+        {
+            Minimum = 0,
+            Maximum = 200,
+            Increment = 5,
+            Value = 50,
+            HorizontalOptions = LayoutOptions.Center
+        };
+        stepper.ValueChanged += Stepper_Size;
+        juhuslik = new Button
+        {
+            Text = "Juhuslik värv",
+            FontSize = 28,
+            FontFamily = "Luffio",
+            TextColor = Colors.Chocolate,
+            BackgroundColor = Colors.Beige,
+            CornerRadius = 10,
+            HeightRequest = 50,
+            WidthRequest = 200
+        };
+        juhuslik.Clicked += juhuslikVarv;
+        al = new AbsoluteLayout { Children = { label,labelR, slider1,labelG,slider2,labelB,slider3,stepper,juhuslik } };
+        List<View> controls = new List<View> { label,labelR, slider1,labelG,slider2,labelB,slider3,stepper,juhuslik };
         for (int i = 0; i < controls.Count; i++)
         {
-            double yKoht = 0.2 + i * 0.2;
-            AbsoluteLayout.SetLayoutBounds(controls[i], new Rect(0.5, yKoht, 300, 60));
+            double yKoht = 0.1 + i * 0.1;
+            AbsoluteLayout.SetLayoutBounds(controls[i], new Rect(0.5, yKoht, 100, 60));
             AbsoluteLayout.SetLayoutFlags(controls[i], Microsoft.Maui.Layouts.AbsoluteLayoutFlags.PositionProportional);
         }
         Content = al;
     }
     private void Slider_Color(object? sender, ValueChangedEventArgs e)
     {
-        double r = Convert.ToInt32(slider1.Value);
-        double g = Convert.ToInt32(slider2.Value);
-        double b = Convert.ToInt32(slider3.Value);
+        double r =slider1.Value / 255.0;
+        double g =slider2.Value / 255.0;
+        double b =slider3.Value / 255.0;
         label.BackgroundColor = Color.FromRgb(r, g, b);
-        labelR.Text = Convert.ToString(r);
-        labelG.Text = Convert.ToString(g);
-        labelB.Text = Convert.ToString(b);
+        labelR.BackgroundColor = Color.FromRgb(r, 0, 0);
+        labelG.BackgroundColor = Color.FromRgb(0, g, 0);
+        labelB.BackgroundColor = Color.FromRgb(0, 0, b);
+    }
+    private void Stepper_Size(object? sender, ValueChangedEventArgs e)
+    {
+       double size = stepper.Value;
+        label.WidthRequest = size;
+    }
+    private void juhuslikVarv(object? sender, EventArgs e)
+    {
+        int r = rnd.Next(256);
+        int g = rnd.Next(256);
+        int b = rnd.Next(256);
+        label.BackgroundColor = Color.FromRgb(r, g, b);
+        labelR.BackgroundColor = Color.FromRgb(r, 0, 0);
+        labelG.BackgroundColor = Color.FromRgb(0, g, 0);
+        labelB.BackgroundColor = Color.FromRgb(0, 0, b);
     }
 }
