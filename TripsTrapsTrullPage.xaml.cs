973e40b348a6ea852b636e5e96a7d8b77ce34de4
opilane <opilane@HP-CZC2349HT2>
2026-04-16 10:40:35 +0300

    natuke tehtud

--- TripsTrapsTrullPage.xaml.cs
+++ TripsTrapsTrullPage.xaml.cs
@@ -5,6 +5,8 @@ public partial class TripsTrapsTrullPage : ContentPage
 	Grid gr3x3;
 	Image img;
 	int player;
+    Switch s_pilt;
+
 	public TripsTrapsTrullPage()
 	{
         img = new Image
@@ -19,5 +21,20 @@ public partial class TripsTrapsTrullPage : ContentPage
             IsToggled = true,
             IsEnabled = true
         };
+        gr3x3 = new Grid
+        {
+            Padding = 10,
+            ColumnSpacing = 8,
+            RowSpacing = 8,
+            VerticalOptions = LayoutOptions.Center,
+            HorizontalOptions = LayoutOptions.Center,
+            WidthRequest = 350,
+            HeightRequest = 350
+        };
+        for (int i = 0; i < 3; i++)
+        {
+            gr3x3.RowDefinitions.Add(new RowDefinition { Height = GridLength.Star });
+            gr3x3.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
+        }
     }
 }
