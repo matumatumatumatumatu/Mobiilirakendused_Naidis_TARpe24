2a6596c864a1b1ace7a3c075ae618734b54e8ba4
opilane <opilane@HP-CZC2349HT2>
2026-04-23 10:43:33 +0300

    trips traps trull saavutatud

--- TripsTrapsTrullPage.xaml.cs
+++ TripsTrapsTrullPage.xaml.cs
@@ -6,6 +6,7 @@ public partial class TripsTrapsTrullPage : ContentPage
     Image Ofire;
     Image Xfire;
     int player;
+    int[,] board = new int[3, 3];
     Switch s_pilt;
     VerticalStackLayout vsl;
 
@@ -43,30 +44,26 @@ public partial class TripsTrapsTrullPage : ContentPage
             gr3x3.RowDefinitions.Add(new RowDefinition { Height = GridLength.Star });
             gr3x3.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
         }
+
         Button alusta = new Button
         {
             Text = "Alusta mäng"
         };
         alusta.Clicked += Alusta_Clicked;
 
-
-        Content = vsl;
-
         Content = new VerticalStackLayout
         {
             Children = { alusta, gr3x3 }
         };
-
-
-
-
     }
 
     private void Alusta_Clicked(object? sender, EventArgs e)
     {
-        int Moves;
         gr3x3.Children.Clear();
-        Moves = 0;
+
+        board = new int[3, 3]; 
+        player = 0;            
+
         for (int row = 0; row < 3; row++)
         {
             for (int col = 0; col < 3; col++)
@@ -81,28 +78,89 @@ public partial class TripsTrapsTrullPage : ContentPage
                     FontSize = 32
                 };
 
-                 button.Clicked += Button_Clicked;
+                
+                button.BindingContext = new Tuple<int, int>(row, col);
+                button.Clicked += Button_Clicked;
 
                 gr3x3.Add(button, col, row);
             }
         }
     }
-    private void Button_Clicked(object? sender, EventArgs e)
+
+    private async void Button_Clicked(object? sender, EventArgs e)
     {
+        int winner = 0; 
+        string winnerString;
+
         var button = (Button)sender;
 
         if (button.ImageSource != null) return;
 
+        var pos = (Tuple<int, int>)button.BindingContext;
+        int row = pos.Item1;
+        int col = pos.Item2;
+
         if (player == 0)
         {
             button.ImageSource = "xfire.jpg";
+            board[row, col] = 1;
             player = 1;
         }
         else
         {
             button.ImageSource = "ofire.png";
+            board[row, col] = 2;
             player = 0;
         }
 
+        
+        if (board[0, 0] == board[0, 1] && board[0, 1] == board[0, 2] && board[0, 0] != 0)
+            winner = board[0, 0];
+        else if (board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2] && board[1, 0] != 0)
+            winner = board[1, 0];
+        else if (board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2] && board[2, 0] != 0)
+            winner = board[2, 0];
+
+        
+        else if (board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0] && board[0, 0] != 0)
+            winner = board[0, 0];
+        else if (board[0, 1] == board[1, 1] && board[1, 1] == board[2, 1] && board[0, 1] != 0)
+            winner = board[0, 1];
+        else if (board[0, 2] == board[1, 2] && board[1, 2] == board[2, 2] && board[0, 2] != 0)
+            winner = board[0, 2];
+
+        
+        else if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] && board[0, 0] != 0)
+            winner = board[0, 0];
+        else if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0] && board[0, 2] != 0)
+            winner = board[0, 2];
+
+        if (winner != 0)
+        {
+            winnerString = winner == 1 ? "X" : "O";
+            await DisplayAlert("Teade", "Võitja on: " + winnerString, "OK");
+        }
+        else
+        {
+            bool isDraw = true;
+
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    if (board[r, c] == 0)
+                    {
+                        isDraw = false;
+                        break;
+                    }
+                }
+                if (!isDraw) break;
+            }
+
+            if (isDraw)
+            {
+                await DisplayAlert("Teade", "Viik!", "OK");
+            }
+        }
     }
 }
